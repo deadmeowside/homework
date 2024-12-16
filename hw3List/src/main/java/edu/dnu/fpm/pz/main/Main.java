206d51f0407f87cd8dde62ecb97f06573d23d2a8
Bohdan <94188154+deadmeowside@users.noreply.github.com>
2024-12-16 23:27:51 +0200

    Update Main.java

--- hw3List/src/main/java/edu/dnu/fpm/pz/main/Main.java
+++ hw3List/src/main/java/edu/dnu/fpm/pz/main/Main.java
@@ -9,26 +9,28 @@ public class Main {
         return new CList<>(1);
     }
 
-    public static void main(String[] args) {
-        AbstractList<String> list = getList();
-
+    public static void demoMethod(AbstractList<String> list) {
         try {
             list.push_back("hello");
             list.push_back("world");
             list.push_back("list");
             list.push_back(Main.class.toString());
 
-            System.out.println(list.get(1));
+            System.out.println("Element at index 1: " + list.get(1));
             list.set(1, "123");
-            System.out.println(list.get(1));
+            System.out.println("Updated element at index 1: " + list.get(1));
 
-            System.out.println(list.remove(2));
-            System.out.println(list.size());
+            System.out.println("Removed element at index 2: " + list.remove(2));
+            System.out.println("List size after removal: " + list.size());
 
             list.get(10);
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println("Exception: " + e.getMessage());
         }
+    }
+
+    public static void main(String[] args) {
+        demoMethod(getList());
     }
 
 }
